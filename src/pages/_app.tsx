63d6ae14005dfc59d0bbc2c2a0939cd34854d7a0
Christopher Evans <crevulus@gmail.com>
2023-05-17 14:31:42 +0100

    Add supabase auth; issues with supabase auth types; why do I need a docker container?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
 import { type AppType } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SessionContextProvider,
+  type Session,
+} from "@supabase/auth-helpers-react";
 
 import { api } from "~/utils/api";
+import { type AppRouter } from "~/server/api/root";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const supabaseClient = createBrowserSupabaseClient<AppRouter>();
+
+const MyApp: AppType<{
+  initialSession: Session;
+}> = ({ Component, pageProps }) => {
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
